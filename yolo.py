5dcfa41b65032189027cc84025e456bf0d6da1bc
kr1p01 <kriipaks_jp@outlook.jp>
2025-04-24 23:25:03 +0900

    seventh

--- yolo.py
+++ yolo.py
@@ -4,7 +4,7 @@ import time
 import cv2
 
 pygame.init()
-
+#startなどボタンのクラス
 class button():
     def __init__(self,x,y,width,height,color,text):
         self.rect =pygame.Rect(x,y,width,height)
@@ -29,14 +29,14 @@ class button():
         
 
 def main():
-    width, height = 800, 480
+    width, height = 800, 540
     screen = pygame.display.set_mode((width, height))
 
-    model = YOLO("yolov8x.pt")
+    model = YOLO("yolov8n.pt")
 
     model.to("cuda")
 
-    target=11
+    target=0
 
     target_class_ids = [target]
 
@@ -48,22 +48,26 @@ def main():
     tuuka = font.render("tuuka", True, (255,255,255))
     start = font.render("start", True, (255,255,255))
     stop = font.render("stop", True, (255,255,255))
-    lap_rect =pygame.Rect(640,0,160,height)
+    background =pygame.Rect(0,0,width,height)
     delay = 100
-    laps = [0]
+    laps = []
     x1=0
     y1=0
     y2=0
     x2=0
+    line_color=(0,0,0)
     isrunning = False
+    ready = False
+
     # WEBカメラからリアルタイム検出
-    results = model(1 , show=False, stream = True,classes = [target])#11 stopsign
+    results = model(0 , show=False, stream = True,classes = [target])#11 stopsign
     
-    start = button(380,0,100,40,(0,255,0),"start")
-    stop = button(380,0,100,40,(255,0,0),"stop")
+    start = button(5,5,100,40,(0,255,0),"start")
+    stop = button(5,5,100,40,(255,0,0),"stop")
     
     for result in results:
 
+        #OpenCV部分
         frame = result.orig_img
 
         for detection in result.boxes:
@@ -71,20 +75,30 @@ def main():
             confidence = detection.conf  # 信頼度
             print(class_id)
             if class_id in target_class_ids:
-                print(result.boxes)
+                #print(result.boxes)
                 pos = detection.xyxy[0]  # 座標
                 print(f"座標: {pos}")
                 x1, y1, x2, y2 = map(int, pos)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)  # 青色の四角
 
+        #Pygameに変換・描画
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # 色を変換
         frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
         frame = pygame.surfarray.make_surface(frame)
 
-        screen.blit(frame, (0, 0))
+        screen.fill((255,255,255), background)
+        screen.blit(frame, (0, height-480))
 
-        if isrunning:
-            if x1 < width/2 and x2 > width/2 and delay > 30:
+        #通過判定、スタートボタン表示
+        pygame.draw.line(screen, line_color, (640/2,0), (640/2,height), 5)
+        if ready:
+            line_color = (0,255,255)
+            if x1 < 640/2 and x2 > 640/2:
+                isrunning = True
+                ready = False
+        elif isrunning:
+            line_color = (0,255,0)
+            if x1 < 640/2 and x2 > 640/2 and delay > 30:
                 delay = 0
                 laps.append(tick)
                 screen.blit(tuuka,(100,0))
@@ -93,24 +107,26 @@ def main():
                 isrunning = False
             tick += 1
         else:
+            line_color = (255,0,0)
             start.draw(screen)
             if start.click():
-                isrunning = True
+                ready = True
                 tick = 0
-                laps = [0]
+                laps = []
 
-        text = font.render(f"{tick/10}s", True, (255,255,255))
-        screen.blit(text, (0, 0))
+        #現在タイム、ラップ表示
+        text = font.render(f"{tick/10}s", True, (0,0,0))
+        screen.blit(text, (640, 5))
 
-        lap_txt = font.render(f"{laps[-1]/10}s", True, (255,255,255))
-        screen.fill((255,255,255), lap_rect)
-        pygame.draw.line(screen, (0,95,0), (640/2,0), (640/2,height), 5)
-        for lap in laps:
-            if laps.index(lap)==0:
-                continue
-            lap_txt = font.render(f"{lap/10}s /{laps.index(lap)}", True, (0,0,0))
-            screen.blit(lap_txt,(640,laps.index(lap)*30))
+        if 0 in laps:
+            lap_txt = font.render(f"{laps[-1]/10}s", True, (255,255,255))
+            for lap in laps:
+                if laps.index(lap)==0:
+                    continue
+                lap_txt = font.render(f"{lap/10}s /{laps.index(lap)}", True, (0,0,0))
+                screen.blit(lap_txt,(640,10+laps.index(lap)*30))
         pygame.display.flip()  # 画面を更新
+        print(laps)
 
         delay += 1
         
--- yolov8_cam.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-model = YOLO("yolov8n.pt")
-
-results = model(R"C:\Users\kriip\Documents\innovation\yolo\images\test.mp4", save=True) 
